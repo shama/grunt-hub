de6fb48730a0f46d6bd14ad4b63ccc40c6cdfebd
Kyle Robinson Young <kyle@dontkry.com>
2014-02-04 21:07:50 -0800

    Clean up gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,17 @@
 module.exports = function(grunt) {
-  'use strict';
-
   grunt.initConfig({
     hub: {
       all: {
         src: ['../*/Gruntfile.js'],
-        tasks: ['jshint']
-      }
-    },
-    watch: {
-      all: {
-        files: '<%= hub.all.src %>'
-      }
+        tasks: ['jshint'],
+      },
     },
     jshint: {
-      files: ['Gruntfile.js', 'tasks/*.js'],
-      options: { jshintrc: '.jshintrc' }
-    }
+      files: ['Gruntfile.js', 'tasks/**/*js'],
+      options: { jshintrc: '.jshintrc' },
+    },
   });
-
-  // Load external tasks
   grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  // Load watch and hub tasks
   grunt.loadTasks('tasks');
-
   grunt.registerTask('default', ['jshint']);
 };
