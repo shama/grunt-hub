40f909af5efc4a8e5941da49a966fb727d5a9b57
Kyle Robinson Young <kyle@dontkry.com>
2014-02-11 19:56:58 -0800

    Nitpick reduce code

--- tasks/hub.js
+++ tasks/hub.js
@@ -26,9 +26,8 @@ module.exports = function(grunt) {
     // Get process.argv options without grunt.cli.tasks to pass to child processes
     var cliArgs = _.without.apply(null, [[].slice.call(process.argv, 2)].concat(grunt.cli.tasks));
     // Get it's own gruntfile
-    var ownGruntfile = "";
-    ownGruntfile += grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
-    ownGruntfile = path.resolve(process.cwd(), ownGruntfile);
+    var ownGruntfile += grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
+    ownGruntfile = path.resolve(process.cwd(), ownGruntfile || '');
 
     var lastGruntFileWritten;
     function write(gruntfile, buf, isError) {
