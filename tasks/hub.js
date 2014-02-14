56fc537b537672420011ee2c380ffc5960dca8a5
Kyle Robinson Young <kyle@dontkry.com>
2014-02-13 21:35:10 -0800

    Fix syntax error. Thanks @eugeneiiim!

--- tasks/hub.js
+++ tasks/hub.js
@@ -12,7 +12,7 @@ module.exports = function(grunt) {
   var path = require('path');
   var chalk = require('chalk');
   var async = require('async');
-  var _ = require('lodash')
+  var _ = require('lodash');
 
   grunt.registerMultiTask('hub', 'Run multiple grunt projects', function() {
     var options = this.options({
@@ -26,7 +26,7 @@ module.exports = function(grunt) {
     // Get process.argv options without grunt.cli.tasks to pass to child processes
     var cliArgs = _.without.apply(null, [[].slice.call(process.argv, 2)].concat(grunt.cli.tasks));
     // Get it's own gruntfile
-    var ownGruntfile += grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
+    var ownGruntfile = grunt.option('gruntfile') || grunt.file.expand({filter: 'isFile'}, '{G,g}runtfile.{js,coffee}')[0];
     ownGruntfile = path.resolve(process.cwd(), ownGruntfile || '');
 
     var lastGruntFileWritten;
